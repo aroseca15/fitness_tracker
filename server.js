0325b896265b76eea31b89f1c6d5d752bb755376
aroseca15 <69179642+aroseca15@users.noreply.github.com>
2021-01-04 14:59:28 -0700

    set up heroku done

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness_tracker", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb:27017/localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
